5f3f5bbc063f0db8aa1d453b19ef7f1de291bd69
g-eez <ggkariuki959@gmail.com>
2025-03-29 12:55:30 +0300

    Resolved merge conflict by accepting incoming changes

--- practice.py
+++ practice.py
@@ -1,8 +1,4 @@
 print("Hello World!")
 print("Hello World!2 push")
 print("Hello World!3 pull")
-<<<<<<< HEAD
-print("Hello World!5 without pull")
-=======
 print("Hello World!4 without pull")
->>>>>>> 67d94c7a1100e6713e09ffd6ac46f9eaac0abc5f
